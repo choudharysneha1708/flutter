e80fb7c3e870c876319a65bd183cdcd27d63cb68
choudharysneha1708 <choudharysneha1708@gmail.com>
2025-02-22 21:41:51 +0530

    learn function , data types , scaffold, and material app

--- blood_bank/lib/main.dart
+++ blood_bank/lib/main.dart
@@ -1,5 +1,21 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(home: Text("Hello World")));
+  String name = "Sneha";
+  int a = 12;
+  int b = 89;
+  int sum = add(a, b);
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: Text("Hello World $sum $name"),
+        ),
+      ),
+    ),
+  );
+}
+
+int add(num1, num2) {
+  return num1 + num2;
 }
